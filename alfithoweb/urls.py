3d30000e044dc02e843596144112857916fe7f67
alfitho dinopa <alfithodinopa@gmail.com>
2025-04-17 11:00:34 +0800

    Menambahkan apps pengguna dan berita dan membuat model

--- alfithoweb/urls.py
+++ alfithoweb/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 
+#menampilkan gambar yang sudah di upload di folder media#
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 from alfithoweb.views import home, about, kutipan
 urlpatterns = [
@@ -10,3 +14,7 @@ urlpatterns = [
     path('about/', about, name="about"),
     path('kutipan/', kutipan, name="kutipan"),
 ]
+
+#menampilkan gambar yang sudah di upload di folder media#
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
